27b6f4fb8b39ed43dd4aa0bf78e53a32423c4b76
rainavyas <sayvaniar@gmail.com>
2020-12-13 12:04:06 +0000

    kl distances

--- calculate_kl.py
+++ calculate_kl.py
@@ -8,6 +8,65 @@ import os
 import argparse
 from pkl2pqvects_noI import get_vects, get_phones
 
+def calculate_kl(p_vects, q_vects, p_frames_mask, q_frames_mask):
+    # Get p/q_lengths
+    p_lengths = torch.sum(p_frames_mask[:,:,:,0].squeeze(), dim=2).unsqueeze(dim=2).repeat(1,1,13)
+    q_lengths = torch.sum(q_frames_mask[:,:,:,0].squeeze(), dim=2).unsqueeze(dim=2).repeat(1,1,13)
+
+    p_lengths[p_lengths==0] = 1
+    q_lengths[q_lengths==0] = 1
+
+    # Compute means
+    p_means = torch.sum(p_vects, dim=2)/p_lengths
+    q_means = torch.sum(q_vects, dim=2)/q_lengths
+
+    # Compute the p/q_covariances tensor
+    p_vects_unsq = torch.unsqueeze(p_vects, dim=4)
+    q_vects_unsq = torch.unsqueeze(q_vects, dim=4)
+
+    p_vects_unsq_T = torch.transpose(p_vects_unsq, 3, 4)
+    q_vects_unsq_T = torch.transpose(q_vects_unsq, 3, 4)
+
+    p_means_squared = torch.squeeze(torch.sum(torch.matmul(p_vects_unsq, p_vects_unsq_T), dim=2)/p_lengths.unsqueeze(dim=3).repeat(1,1,1,13))
+    q_means_squared = torch.squeeze(torch.sum(torch.matmul(q_vects_unsq, q_vects_unsq_T), dim=2)/q_lengths.unsqueeze(dim=3).repeat(1,1,1,13))
+
+    p_means_unsq = torch.unsqueeze(p_means, dim=3)
+    q_means_unsq = torch.unsqueeze(q_means, dim=3)
+
+    p_means_unsq_T = torch.transpose(p_means_unsq, 2, 3)
+    q_means_unsq_T = torch.transpose(q_means_unsq, 2, 3)
+
+    p_m2 = torch.squeeze(torch.matmul(p_means_unsq, p_means_unsq_T))
+    q_m2 = torch.squeeze(torch.matmul(q_means_unsq, q_means_unsq_T))
+
+    p_covariances = p_means_squared - p_m2
+    q_covariances = q_means_squared - q_m2
+
+    # If no phone, make covariance matrix identity
+    # Need to first calculate num_phones_mask
+    num_phones_mask_p = p_frames_mask[:,:,0,0].squeeze()
+    num_phones_mask_q = q_frames_mask[:,:,0,0].squeeze()
+    num_phones_mask = num_phones_mask_p + num_phones_mask_q
+    num_phones_mask[num_phones_mask==1]=0
+    num_phones_mask[num_phones_mask==2]=1
+
+    p_covariances_shifted = p_covariances - torch.eye(13)
+    q_covariances_shifted = q_covariances - torch.eye(13)
+
+    p_covariances_shifted_masked = p_covariances_shifted * num_phones_mask.unsqueeze(dim=2).repeat(1,1,13).unsqueeze(dim=3).repeat(1,1,1,13)
+    q_covariances_shifted_masked = q_covariances_shifted * num_phones_mask.unsqueeze(dim=2).repeat(1,1,13).unsqueeze(dim=3).repeat(1,1,1,13)
+
+    p_covs = p_covariances_shifted_masked + torch.eye(13)
+    q_covs = q_covariances_shifted_masked + torch.eye(13)
+
+    # Calculate the symmetric KL divergences
+    p = torch.distributions.MultivariateNormal(p_means, p_covs)
+    q = torch.distributions.MultivariateNormal(q_means, q_covs)
+
+    kl_loss = ((torch.distributions.kl_divergence(p, q) + torch.distributions.kl_divergence(q, p))*0.5)
+
+    return kl_loss
+
 # Get command line arguments
 commandLineParser = argparse.ArgumentParser()
 commandLineParser.add_argument('PKL', type=str, help='Specify input pkl file')
--- training_siamese_kl_noI.py
+++ training_siamese_kl_noI.py
@@ -10,75 +10,17 @@ from pkl2pqvects_noI import get_vects, get_phones
 from model_siamese_noI import Siamese
 from utility import calculate_mse
 
-
-def calculate_kl(p_vects, q_vects, p_frames_mask, q_frames_mask):
-    # Get p/q_lengths
-    p_lengths = torch.sum(p_frames_mask[:,:,:,0].squeeze(), dim=2).unsqueeze(dim=2).repeat(1,1,13)
-    q_lengths = torch.sum(q_frames_mask[:,:,:,0].squeeze(), dim=2).unsqueeze(dim=2).repeat(1,1,13)
-
-    p_lengths[p_lengths==0] = 1
-    q_lengths[q_lengths==0] = 1
-
-    # Compute means
-    p_means = torch.sum(p_vects, dim=2)/p_lengths
-    q_means = torch.sum(q_vects, dim=2)/q_lengths
-
-    # Compute the p/q_covariances tensor
-    p_vects_unsq = torch.unsqueeze(p_vects, dim=4)
-    q_vects_unsq = torch.unsqueeze(q_vects, dim=4)
-
-    p_vects_unsq_T = torch.transpose(p_vects_unsq, 3, 4)
-    q_vects_unsq_T = torch.transpose(q_vects_unsq, 3, 4)
-
-    p_means_squared = torch.squeeze(torch.sum(torch.matmul(p_vects_unsq, p_vects_unsq_T), dim=2)/p_lengths.unsqueeze(dim=3).repeat(1,1,1,13))
-    q_means_squared = torch.squeeze(torch.sum(torch.matmul(q_vects_unsq, q_vects_unsq_T), dim=2)/q_lengths.unsqueeze(dim=3).repeat(1,1,1,13))
-
-    p_means_unsq = torch.unsqueeze(p_means, dim=3)
-    q_means_unsq = torch.unsqueeze(q_means, dim=3)
-
-    p_means_unsq_T = torch.transpose(p_means_unsq, 2, 3)
-    q_means_unsq_T = torch.transpose(q_means_unsq, 2, 3)
-
-    p_m2 = torch.squeeze(torch.matmul(p_means_unsq, p_means_unsq_T))
-    q_m2 = torch.squeeze(torch.matmul(q_means_unsq, q_means_unsq_T))
-
-    p_covariances = p_means_squared - p_m2
-    q_covariances = q_means_squared - q_m2
-
-    # If no phone, make covariance matrix identity
-    # Need to first calculate num_phones_mask
-    num_phones_mask_p = p_frames_mask[:,:,0,0].squeeze()
-    num_phones_mask_q = q_frames_mask[:,:,0,0].squeeze()
-    num_phones_mask = num_phones_mask_p + num_phones_mask_q
-    num_phones_mask[num_phones_mask==1]=0
-    num_phones_mask[num_phones_mask==2]=1
-
-    p_covariances_shifted = p_covariances - torch.eye(13)
-    q_covariances_shifted = q_covariances - torch.eye(13)
-
-    p_covariances_shifted_masked = p_covariances_shifted * num_phones_mask.unsqueeze(dim=2).repeat(1,1,13).unsqueeze(dim=3).repeat(1,1,1,13)
-    q_covariances_shifted_masked = q_covariances_shifted * num_phones_mask.unsqueeze(dim=2).repeat(1,1,13).unsqueeze(dim=3).repeat(1,1,1,13)
-
-    p_covs = p_covariances_shifted_masked + torch.eye(13)
-    q_covs = q_covariances_shifted_masked + torch.eye(13)
-
-    # Calculate the symmetric KL divergences
-    p = torch.distributions.MultivariateNormal(p_means, p_covs)
-    q = torch.distributions.MultivariateNormal(q_means, q_covs)
-
-    kl_loss = ((torch.distributions.kl_divergence(p, q) + torch.distributions.kl_divergence(q, p))*0.5)
-
-    return kl_loss
-
 # Get command line arguments
 commandLineParser = argparse.ArgumentParser()
 commandLineParser.add_argument('PKL', type=str, help='Specify input pkl file')
+commandLineParser.add_argument('KL', type=str, help='Specify labels pkl file with kl distances')
 commandLineParser.add_argument('OUT', type=str, help='Specify output pt file')
 commandLineParser.add_argument('--N', default=993, type=int, help='Specify number of speakers')
 commandLineParser.add_argument('--F', default=1000, type=int, help='Specify maximum number of frames in phone instance')
 
 args = commandLineParser.parse_args()
 pkl_file = args.PKL
+kl_file = args.KL
 out_file = args.OUT
 N = args.N
 F = args.F
@@ -105,8 +47,10 @@ M1 = torch.from_numpy(M1).float()
 M2 = torch.from_numpy(M2).float()
 
 # Get the output kl-distance values
-y = calculate_kl(X1, X2, M1, M2)
-print("Calculated KL distances")
+y = pickle.load(open(kl_file, "rb"))
+y = torch.FloatTensor(y)
+y = y[:N]
+print("Got KL distances")
 
 # Split into training and validation sets
 validation_size = 50
@@ -118,7 +62,7 @@ M1_train = M1[validation_size:]
 M1_val = M1[:validation_size]
 M2_train = M2[validation_size:]
 M2_val = M2[:validation_size]
-y_train = y[validation_size:N]
+y_train = y[validation_size:]
 y_val = y[:validation_size]
 
 # Define training constants
